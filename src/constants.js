fa66a484a471d6a06c78ba118f368389fdf3275a
dpchamps <me@davecampion.com>
2021-11-11 18:11:43 -0800

    start

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+import {StaticImage} from "gatsby-plugin-image";
+import * as React from "react"
+
+export const epoch = -770947200000; // jim davis' birthday
+
+export const phases = {
+    garfield: {
+        image: () => {
+            return <StaticImage src={"../images/garfield.jpg"}/>
+        },
+        description: "The garfield phase",
+        title: "Garfield"
+    },
+    odie: {
+        image: () => {
+            return <StaticImage src={"../images/odie.png"}/>
+        },
+        description: "The odie phase",
+        title: "Odie"
+    },
+
+};
