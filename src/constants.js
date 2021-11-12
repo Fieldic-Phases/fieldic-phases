0d42caa96a482ad712fcd1c87dc0aaeff34db2d3
dpchamps <me@davecampion.com>
2021-11-11 18:41:53 -0800

    add stock phases

--- src/constants.js
+++ src/constants.js
@@ -18,5 +18,39 @@ export const phases = {
         description: "The odie phase",
         title: "Odie"
     },
-
+    jon: {
+        image: () => {
+            return <StaticImage src={"./images/jon.png"}/>
+        },
+        description: "The jon phase",
+        title: "Jon Arbuckle"
+    },
+    nermal: {
+        image: () => {
+            return <StaticImage src={"./images/nermal.jpg"}/>
+        },
+        description: "The nermal phase",
+        title: "Nermal"
+    },
+    drliz: {
+        image: () => {
+            return <StaticImage src={"./images/liz.jpeg"}/>
+        },
+        description: "The Dr Liz Wilson phase",
+        title: "Dr. Liz Wilson"
+    },
+    arlene: {
+        image: () => {
+            return <StaticImage src={"./images/arlene.png"}/>
+        },
+        title: "Arlene",
+        description: "The Arlene phase"
+    },
+    pookie: {
+        image: () => {
+            return <StaticImage src={"./images/pookie.jpeg"}/>
+        },
+        title: "Pookie",
+        description: "The pookie phase"
+    }
 };
